567b19328f03396785abefb6a71234ca0a443afa
muemakisavi618 <151131474+muemakisavi618@users.noreply.github.com>
2023-11-17 12:07:23 +0300

    Add files via upload

---
+++ StudentDetail.java
@@ -0,0 +1,10 @@
+public class StudentDetails{
+public static void main(String[]args){
+System.out.println ("course:DIT");
+System.out.println ("reg number:22/06759");
+System.out.println ("students Name:Angela muema");
+System.out.println ("semester:IV");
+System.out.println ("units taken:Five");
+System.out.println ("dob:30th sep 2003");
+}
+}

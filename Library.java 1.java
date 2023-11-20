f44939a318a116d17a7622fbbc5fc380f4381e58
muemakisavi618 <151131474+muemakisavi618@users.noreply.github.com>
2023-11-20 22:22:12 +0300

    Add files via upload

---
+++ Library.java 1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Library {
+
+    public static void main(String[]args){
+        
+
+        System.out.println("title:Good samaritan");
+        System.out.println("yearofpublishing:2010");
+        System.out.println("author:wilberforce");
+        System.out.println("LC call no:1110");
+        System.out.println("Campus:KCA university");
+    }
+}
